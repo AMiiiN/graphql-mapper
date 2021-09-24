59db7f57ac7260706d27212a795a3a336190b7a2
Amin Harig <aminharig@me.com>
2021-09-24 17:09:25 +0200

    First working version of simple SQL -> GraphQL translation. (simple SELECT/FROM statements)

--- translateTests/sqltogql.js
+++ translateTests/sqltogql.js
@@ -1,10 +1,12 @@
 const keywords = [
   'SELECT',
-  'FROM'
+  'FROM',
+  'WHERE'
 ];
 
 const queries = [
-  "SELECT salary FROM instructor"
+  "SELECT salary FROM instructor",
+  "SELECT salary, name FROM instructor"
 ];
 
 var gqlQueries = [];
@@ -22,9 +24,21 @@ for (var i=0; i < queries.length; i++) {
   key_select = tempSplit[0].replace(/\s/g, '');
   key_from = tempSplit[1].replace(/\s/g, '');
 
+  // Check whether SELECT (multiple attributes) separated by commata
+  if (key_select.includes(',')) {
+    var temp = key_select.split(',');
+    key_select = '';
+    for (var a=0; a<temp.length; a++) {
+      key_select += temp[a];
+      if (a != temp.length-1) {
+        key_select += ', ';
+      }
+    }
+  }
+
   gqlQuery = "{ " + key_from + " { " + key_select + " } }";
 
-  console.log("Original query: " + queries[i]);
-  console.log("GraphQL query : " + gqlQuery);
+  console.log("Original query " + i + ": " + queries[i]);
+  console.log("GraphQL query " + i + " : " + gqlQuery);
 
 }
