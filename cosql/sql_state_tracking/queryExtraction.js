275c82bc9c2402e0690494d0ed7ec871898b7140
AMiiiN <amin.laaouissi@gmail.com>
2021-09-30 22:58:18 +0200

    Finished implementation of training data extraction

--- cosql/sql_state_tracking/queryExtraction.js
+++ cosql/sql_state_tracking/queryExtraction.js
@@ -3,6 +3,8 @@ const training_json = require('./cosql_train.json');
 
 var totalCountQueries = 0;
 var entireJSON = {};
+var queriesJSON = [];
+entireJSON.queries = queriesJSON;
 
 var currentJSON = {};
 for (var i=0; i<training_json.length; i++) {
@@ -12,15 +14,24 @@ for (var i=0; i<training_json.length; i++) {
     "query": finalQuery,
     "databaseID": currentDBID
   };
-  entireJSON.push(currentJSON);
+  entireJSON.queries.push(currentJSON);
   //console.log(training_json[i].final.query);
   totalCountQueries++;
 
   for (var j=0; j<training_json[i].interaction.length; j++) {
     //console.log(training_json[i].interaction[j].query);
+    finalQuery = training_json[i].interaction[j].query;
+    currentJSON = {
+      "query": finalQuery,
+      "databaseID": currentDBID
+    };
+    entireJSON.queries.push(currentJSON);
     totalCountQueries++;
   }
 
 }
 
+var export_filename = 'extracted_train.json';
+fs.writeFileSync(export_filename, JSON.stringify(entireJSON, null, 3));
 console.log("-- Total extracted queries: " + totalCountQueries);
+console.log("File saved as " + export_filename);
