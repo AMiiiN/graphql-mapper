41e0e945fe04771bcaea5e2667cc7afd99392e4e
AMiiiN <amin.laaouissi@gmail.com>
2021-11-01 17:17:18 +0100

    Extraction of all natural languages queries related to the "aircrafts" database

--- cosql/sql_state_tracking/queryExtraction.js
+++ cosql/sql_state_tracking/queryExtraction.js
@@ -6,10 +6,15 @@ var entireJSON = {};
 var queriesJSON = [];
 entireJSON.queries = queriesJSON;
 
+const desired_database = "aircraft";
+
 var currentJSON = {};
 for (var i=0; i<training_json.length; i++) {
-  var finalQuery = training_json[i].final.query;
+  var finalQuery = training_json[i].final.utterance;
   var currentDBID = training_json[i].database_id;
+  if (currentDBID != desired_database) {
+    continue;
+  }
   currentJSON = {
     "query": finalQuery,
     "databaseID": currentDBID
@@ -20,7 +25,7 @@ for (var i=0; i<training_json.length; i++) {
 
   for (var j=0; j<training_json[i].interaction.length; j++) {
     //console.log(training_json[i].interaction[j].query);
-    finalQuery = training_json[i].interaction[j].query;
+    finalQuery = training_json[i].interaction[j].utterance;
     currentJSON = {
       "query": finalQuery,
       "databaseID": currentDBID
@@ -31,7 +36,7 @@ for (var i=0; i<training_json.length; i++) {
 
 }
 
-var export_filename = 'extracted_train.json';
+var export_filename = 'extracted_queries_aircraft.json';
 fs.writeFileSync(export_filename, JSON.stringify(entireJSON, null, 3));
 console.log("-- Total extracted queries: " + totalCountQueries);
 console.log("File saved as " + export_filename);
